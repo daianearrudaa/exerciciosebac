9e6dacfb92d0077b2c617d430458c9c50681e88a
Daiane Arruda <daianedasilvaarruda@outlook.com>
2023-12-14 12:38:50 -0300

    alteracao exercicio

--- src/exercicio.ts
+++ src/exercicio.ts
@@ -1,12 +1,12 @@
 
 
 function calcular(n1: number, n2: number): number {
-    return n1 + n2;
+    return n1 * n2;
 }
 
 const resultado = calcular(3, 5);
 
-console.log("O resultado da soma é:", resultado);
+console.log("O resultado da multiplicação é:", resultado);
 
 // Função de saudação
 function saudacao(nome: string): string {
